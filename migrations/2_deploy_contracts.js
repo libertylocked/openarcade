42b4d6eb055cde39cf57570181bd667fdb714d76
libertylocked <unlock.liberty@gmail.com>
2018-06-26 01:15:51 -0400

    migration lib link fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ const TTTController = artifacts.require("./TTTController.sol")
 
 module.exports = (deployer, network, accounts) => {
   deployer.deploy(PlatformLib)
-  deployer.link(PlatformLib, TTTController)
+  deployer.link(PlatformLib, [TTTLib, TTTController])
   deployer.deploy(TTTLib)
   deployer.link(TTTLib, TTTController)
   deployer.deploy(TTTController, accounts[0], accounts[1])
