b1a54038c3e79f71e84ba5727de6d3a74b57cafd
libertylocked <unlock.liberty@gmail.com>
2018-07-03 16:32:29 -0400

    add eslint for migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const Util = artifacts.require("./Util.sol")
-const Controller = artifacts.require("./Controller.sol")
-const Game = artifacts.require("./TTTGame.sol")
+const Util = artifacts.require('./Util.sol')
+const Controller = artifacts.require('./Controller.sol')
+const Game = artifacts.require('./TTTGame.sol')
 
 module.exports = (deployer, network, accounts) => {
   deployer.deploy(Util)
