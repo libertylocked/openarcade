391c5286c2b267c189dac5b531ba5a1b68977c52
libertylocked <unlock.liberty@gmail.com>
2018-07-20 21:12:52 -0400

    add starter lib, update readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-Ethereum Open Arcade
+OpenArcade
 ---
+
+# What is OpenArcade?
+OpenArcade is a game engine for turn based games on Ethereum.
+
+## Features
+- Easy and simple - You only need to implement an interface of a few functions to get going
+- Library driven - Your game logic is only deployed once, saving gas
+- Batteries included - It comes with many useful utils suchs as a random number generator
+- Out-of-the-box state channel support - Faster and cheaper matches for your game
+- Play for money (or not) - OpenArcade supports high stake competitive games
+
+## Examples
+- See `contracts/games` for example games developed on OpenArcade!
+- See `test` folder for client example code
+
+# Quick start
+- Copy the starter template at `contracts/games/Starter.sol` to `contracts/games/MyGame.sol`
+- Implement the functions marked as TODOs
+- Run `npm run compile`
+- Write some tests! You can require your game controller like this in tests
+```javascript
+const Controller = artifacts.require('DiceController')
+```
+- Put the tests in `test` folder
+- Run `npm run test`
+
+# Developer Guides
+- [Make your first OpenArcade game](#)
+- [Common pitfalls](#)
---
+++ contracts/games/Starter.sol
@@ -0,0 +1,83 @@
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../util/BytesUtil.sol";
+// TODO: Edit this line for your game! e.g. MyGameConnect
+import { StarterConnect as Connect } from "../.generated/StarterConnect.sol";
+
+
+library Starter {
+    using SafeMath for uint256;
+    using BytesUtil for bytes;
+
+    struct State {
+    }
+
+    struct Action {
+    }
+
+    /* Internal functions */
+
+    function init(State storage state, Connect.Tools storage tools, uint playerCount)
+        internal
+        returns (uint)
+    {
+        // TODO
+        return 0;
+    }
+
+    function next(State storage state, Connect.Info storage info)
+        internal view
+        returns (uint)
+    {
+        // TODO
+        return 0;
+    }
+
+    function update(State storage state, Connect.Tools storage tools, Connect.Info storage info, Connect.Input memory input)
+        internal
+    {
+        // TODO
+    }
+
+    function legal(State storage state, Connect.Info storage info, Connect.Input memory input)
+        internal view
+        returns (bool)
+    {
+        // TODO
+        return true;
+    }
+
+    function terminal(State storage state, Connect.Info storage info)
+        internal view
+        returns (bool)
+    {
+        // TODO
+        return false;
+    }
+
+    function goal(State storage state, Connect.Info storage info, uint pid)
+        internal view
+        returns (uint)
+    {
+        // TODO
+        return 0;
+    }
+
+    function decodeAction(bytes s)
+        internal pure
+        returns (Action)
+    {
+        // TODO
+        return Action();
+    }
+
+    /* External functions */
+
+    function encodeAction(bool play)
+        external pure
+        returns (bytes)
+    {
+        return abi.encode(play);
+    }
+}
