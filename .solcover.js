7123039adf4782c4dd545d438536afb2edb6b71e
libertylocked <unlock.liberty@gmail.com>
2018-07-24 22:55:34 -0400

    Fix coverage

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,12 @@
 module.exports = {
   port: 8555,
   copyPackages: ['openzeppelin-solidity'],
-  skipFiles: ['Migrations.sol'],
-  compileCommand: '../node_modules/.bin/truffle compile',
+  skipFiles: [
+    'Migrations.sol',
+    'games/Starter.sol',
+    'mocks/BytesUtilMock.sol',
+    'mocks/ConnectMock.sol',
+  ],
+  compileCommand: 'npm run compile',
   testCommand: '../node_modules/.bin/truffle test --network coverage',
 };
--- test/random/TableRandom_test.js
+++ test/random/TableRandom_test.js
@@ -16,38 +16,38 @@ contract('TableRandom', () => {
   })
   describe('ready', () => {
     it('should always return true', async () => {
-      assert.isTrue(await instance.ready())
+      assert.isTrue(await instance.ready.call())
       // try calling next
       await instance.next()
-      assert.isTrue(await instance.ready())
+      assert.isTrue(await instance.ready.call())
       // try calling request
       await instance.request()
-      assert.isTrue(await instance.ready())
+      assert.isTrue(await instance.ready.call())
     })
   })
   describe('request', () => {
     it('should reset index to zero', async () => {
       await instance.next()
       await instance.request()
-      assert.equal(await instance.index(), 0)
+      assert.equal(await instance.index.call(), 0)
     })
   })
   describe('current', () => {
     it('should return the first number in table if next is not called', async () => {
-      assert.equal(await instance.current(), 1)
+      assert.equal(await instance.current.call(), 1)
     })
     it('should return the current number in table as cursor moves', async () => {
       await instance.next()
-      assert.equal(await instance.current(), 2)
+      assert.equal(await instance.current.call(), 2)
       await instance.next()
-      assert.equal(await instance.current(), 3)
+      assert.equal(await instance.current.call(), 3)
     })
     it('should wrap around', async () => {
       await instance.next()
       await instance.next()
       await instance.next()
       await instance.next()
-      assert.equal(await instance.current(), 1)
+      assert.equal(await instance.current.call(), 1)
     })
   })
 })
