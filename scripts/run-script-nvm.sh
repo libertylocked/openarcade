317f31ed3e6a74f16e32f8ae788affc3ebc13378
libertylocked <unlock.liberty@gmail.com>
2018-08-13 16:44:34 -0400

    Use 2 spaces for shell scripts

--- scripts/run-script-nvm.sh
+++ scripts/run-script-nvm.sh
@@ -2,8 +2,8 @@
 NPM_PREFIX=$(npm config get prefix)
 npm config delete prefix
 source /usr/share/nvm/init-nvm.sh --install && \
-    nvm install && \
-    nvm use --delete-prefix && \
-    npm run $1 && \
-    nvm unalias default
+  nvm install && \
+  nvm use --delete-prefix && \
+  npm run $1 && \
+  nvm unalias default
 npm config set prefix ${NPM_PREFIX}
